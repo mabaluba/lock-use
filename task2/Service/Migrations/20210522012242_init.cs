e6e478cb45ee7841064941bf771c58a88f005d59
Sergey Rodionov <srodionov@paysend.com>
2021-05-24 13:08:05 +0300

    add migrations

---
+++ task2/Service/Migrations/20210522012242_init.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using Service;
+
+namespace Service.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:transaction_direction", "income,outcome")
+                .Annotation("Npgsql:Enum:transaction_status", "pending,completed,declined")
+                .Annotation("Npgsql:Enum:transaction_type", "transfer,fee,fx");
+
+            migrationBuilder.CreateTable(
+                name: "accounts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    balance = table.Column<decimal>(type: "numeric", nullable: false),
+                    currency = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_accounts", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transactions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    direction = table.Column<TransactionDirection>(type: "transaction_direction", nullable: false),
+                    type = table.Column<TransactionType>(type: "transaction_type", nullable: false),
+                    status = table.Column<TransactionStatus>(type: "transaction_status", nullable: false),
+                    currency = table.Column<string>(type: "text", nullable: true),
+                    amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    account_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_transactions", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "accounts");
+
+            migrationBuilder.DropTable(
+                name: "transactions");
+        }
+    }
+}
---
+++ task2/Service/Migrations/20210522014554_init2.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Service;
+
+namespace Service.Migrations
+{
+    public partial class init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "accounts",
+                columns: new[] { "id", "balance", "currency", "name" },
+                values: new object[,]
+                {
+                    { 1, 25000m, "GBP", "Main Fund" },
+                    { 2, 1000m, "GBP", "For investment" },
+                    { 3, 150m, "CAN", "For Canada market" },
+                    { 4, 50m, "USD", "For USA market" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "transactions",
+                columns: new[] { "id", "account_id", "amount", "currency", "direction", "status", "type" },
+                values: new object[,]
+                {
+                    { 14, 2, 5.0m, "GBP", TransactionDirection.Income, TransactionStatus.Completed, TransactionType.Transfer },
+                    { 13, 2, 5.0m, "GBP", TransactionDirection.Outcome, TransactionStatus.Completed, TransactionType.Transfer },
+                    { 12, 2, 100.0m, "GBP", TransactionDirection.Outcome, TransactionStatus.Declined, TransactionType.Transfer },
+                    { 11, 2, 20.0m, "GBP", TransactionDirection.Income, TransactionStatus.Declined, TransactionType.Transfer },
+                    { 10, 1, 12000.81m, "GBP", TransactionDirection.Outcome, TransactionStatus.Pending, TransactionType.Transfer },
+                    { 9, 1, 0.15m, "GBP", TransactionDirection.Outcome, TransactionStatus.Declined, TransactionType.Fee },
+                    { 8, 1, 12.52m, "GBP", TransactionDirection.Outcome, TransactionStatus.Declined, TransactionType.Transfer },
+                    { 6, 3, 1000.0m, "USD", TransactionDirection.Income, TransactionStatus.Declined, TransactionType.Transfer },
+                    { 15, 2, 200.0m, "GBP", TransactionDirection.Outcome, TransactionStatus.Completed, TransactionType.Fee },
+                    { 5, 3, 20.0m, "CAN", TransactionDirection.Outcome, TransactionStatus.Pending, TransactionType.FX },
+                    { 4, 3, 0.25m, "CAN", TransactionDirection.Outcome, TransactionStatus.Pending, TransactionType.Fee },
+                    { 3, 4, 0.05m, "USD", TransactionDirection.Income, TransactionStatus.Pending, TransactionType.Fee },
+                    { 2, 4, 16.49m, "USD", TransactionDirection.Income, TransactionStatus.Pending, TransactionType.FX },
+                    { 1, 4, 500m, "USD", TransactionDirection.Outcome, TransactionStatus.Completed, TransactionType.Transfer },
+                    { 7, 1, 50.0m, "GBP", TransactionDirection.Income, TransactionStatus.Pending, TransactionType.Transfer },
+                    { 16, 2, 0.25m, "GBP", TransactionDirection.Outcome, TransactionStatus.Pending, TransactionType.Fee }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "accounts",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "accounts",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "accounts",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "accounts",
+                keyColumn: "id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "transactions",
+                keyColumn: "id",
+                keyValue: 16);
+        }
+    }
+}
